b44ef494bdb6d53bfd07787451f31be6296f21f9
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-12-02 18:50:02 +0200

    feat: add TextInput component
    TextInput component to be used to add new todos

---
+++ src/components/text-input.component.jsx
@@ -0,0 +1,5 @@
+import '../styles/text-input.styles.css'
+const TextInput = ({placeholder}) => {  
+    return <input type='text' className='main-text-input' placeholder={placeholder}/> 
+}
+export default TextInput;
