651ef6089308cb9c5e99acd2098307321c14e122
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-12-02 23:43:48 +0200

    refactor: delete dummy files

--- src/components/button.component.jsx
+++
@@ -1,8 +0,0 @@
-import '../styles/button.styles.css'
-
-const Button = ({text}) => {
-    return (
-        <button type="button"> {text} </button>
-    )
-}
-export default Button;
--- src/components/footer.component.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/footer.styles.css'
-const Footer = ({text}) => {
-    return (
-        <footer>
-            <p className='footer-text'> {text} </p>
-        </footer>
-    )
-}
-export default Footer;
