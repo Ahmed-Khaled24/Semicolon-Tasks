51c9f2d818735a7af1f7036c6ffc0b11c845c461
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-12-17 20:56:59 +0200

    feat: create loadingScreen component

---
+++ src/components/loading-screen/loading-screen.component.jsx
@@ -0,0 +1,11 @@
+import './loading-screen.style.css';
+
+const LoadingScreen = () =>{
+    return (
+        <div className='loader-container'>
+            <div className='loader'></div>
+        </div>
+    )
+}
+
+export default LoadingScreen;
